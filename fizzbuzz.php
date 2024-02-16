8c9fc19894e6b86e1f1c221eb36ea17052a6e2c4
iamgungunnsharma <160225657+iamgungunnsharma@users.noreply.github.com>
2024-02-16 15:29:43 +0530

    Add files via upload

---
+++ fizzbuzz.php
@@ -0,0 +1,17 @@
+<?php
+for($i=1;$i<=50;$i++)
+{
+    if($i%3==0 && $i%5==0)
+    {
+        echo"fizzbuzz<br>";
+    }
+}
+elseif($i%3==o)
+{
+    echo"fizz<br>";
+}
+elseif($i%5==0)
+{
+    echo"buzz<br>";
+}
+?>
